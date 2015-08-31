631053566be8ebae6c94e3683721b28966cd5f8e
Taylor Mott <taylor@mottapplications.com>
2015-08-31 09:20:40 -0600

    Initial Commit

---
+++ MyStoreApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyStoreApp
+//
+//  Created by Taylor Mott on 8/31/15.
+//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
